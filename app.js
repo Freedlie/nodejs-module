1e8d6186f5e1564fd637156f37a5bc565a1e7f88
Freedlie <kolich.m2003@gmail.com>
2023-01-19 11:49:09 +0200

    hw2 p2

--- app.js
+++ app.js
@@ -1,65 +1,79 @@
 const express = require('express');
-const userDB = require('./dataBase/users');
+const {fileServices} = require('./services');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.get('/users',(req,res)=>{
-
-    res.json(userDB)
+app.get('/users', async (req,res)=>{
+    const users = await fileServices.reader();
+    res.json(users)
 })
 
-app.get('/users/:userId',(req,res)=>{
+app.get('/users/:userId',async (req,res)=>{
+
+    const users = await fileServices.reader();
 
     const {userId} = req.params;
 
-    if(userId > userDB.length - 1){
+    if(userId > users.length - 1){
         res.json('not found');
     } else{
-        res.json(userDB[userId]);
-
+        res.json(users[userId - 1]);
     }
 
 })
 
-app.post('/users',(req,res)=>{
+app.post('/users', async(req,res)=>{
     const userInfo = req.body;
 
-    if(typeof userInfo.name === "string" || typeof userInfo.age === "number"){
-        userDB.push(userInfo);
+    const users = await fileServices.reader()
 
-        res.status(201).json('created');
-    }else{
-        res.status(101).json('invalid data');
+    if(typeof userInfo.name !== "string" || typeof +userInfo.age !== "number"){
+        return  res.status(400).json('invalid data');
     }
+
+    const newUser = {
+        name: userInfo.name,
+        age: userInfo.age,
+        id: users[users.length - 1].id +1
+    }
+    users.push(newUser);
+
+    await fileServices.writer(users);
+
+    res.status(201).json(newUser)
 
 })
 
-app.put('/users/:userId',(req,res)=>{
+app.put('/users/:userId',async (req,res)=>{
     const {userId} = req.params;
+
+    const users = await fileServices.reader();
 
     const newUserInfo = req.body;
 
-    if(userId > userDB.length - 1){
-        res.json('not found');
+    if(userId > users.length - 1){
+        return res.json('not found');
     }
-    else if(typeof newUserInfo.name === "string" || typeof newUserInfo.age === "number"){
-        userDB[userId] = newUserInfo;
-        res.json('updated')
+    if(typeof newUserInfo.name !== "string" || typeof +newUserInfo.age !== "number"){
+        return res.status(400).json('invalid data');
     }
-    else{
-        res.status(101).json('invalid data');
-    }
+
+    const updateUser = users[userId] = newUserInfo;
 
 
 })
 
-app.delete('/users/:userId',(req,res)=>{
+app.delete('/users/:userId', async (req,res)=>{
     const {userId} = req.params;
 
-    userDB[userId] = null;
+    const users = await fileServices.reader();
+
+    users.splice(userId, 1);
+
+    await fileServices.writer(users);
 
     res.json('deleted')
 })
@@ -67,5 +81,7 @@ app.delete('/users/:userId',(req,res)=>{
 app.listen(5000,()=>{
     console.log('server listen 5000');
 });
+
+
 
 
